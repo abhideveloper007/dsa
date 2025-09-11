7629ad952b6a12bb0bd5ab3aabe493e0cff0baeb
abhideveloper007 <148200371+abhideveloper007@users.noreply.github.com>
2025-09-12 00:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (63.13%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    int checkpan(vector<int>& ans) {
-        int res = 1;   // assume true, prove false if missing
-        int i = 0;
-        while (i < 26) {
-            if (ans[i] == 0) {
-                res = 0;   // letter missing
-                break;     // no need to check further
-            }
-            i++;
+int  checkpan(vector<int>&ans){
+    int res=1;
+    int i=0;
+    while(i<26){
+        if(ans[i]>0){
+      res=1;       
         }
-        return res;
+        else{
+            res=0;
+            break;
+        }
+        i++;
     }
-
+    return res;
+    
+}
     bool checkIfPangram(string sentence) {
-        vector<int> ans(26, 0);
-        int i = 0;
+        vector<int>ans(26,0);
+        int i=0;
+        // int count=0;
+        while(i<sentence.size()){
+            int pos=sentence[i]-'a';
+            
+            ans[pos]=ans[pos]+1;
         
-        while (i < sentence.size()) {
-            int pos = sentence[i] - 'a';
-            ans[pos] = ans[pos] + 1;
             i++;
-        }
 
-        if (checkpan(ans)) return true;
+        }
+        if(checkpan(ans)) return true;
         else return false;
     }
 };
