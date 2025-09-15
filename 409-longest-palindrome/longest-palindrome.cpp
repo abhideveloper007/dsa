fa1f3747b4c9b8f0a4b67217f65206841f03b194
abhideveloper007 <148200371+abhideveloper007@users.noreply.github.com>
2025-09-15 12:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (61.12%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-    vector<int>upper(26,0);
-    vector<int>lower(26,0);
-    for(int i=0;i<s.size();i++){
-        if(s[i]>='a'){
-            lower[s[i]-'a']++;
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        int i=0;
+        while(i<s.size()){
+            if(s[i]>='a'){
+                int pos=s[i]-'a';
+                lower[pos]++;
+            }
+            else{
+              int  posi=s[i]-'A';
+              upper[posi]++;
 
+            }
+            i++;
         }
-        else{
-            upper[s[i]-'A']++;
+        int odd=0;
+        int count=0;
+        for(int i=0;i<26;i++){
+            // odd
+            if(lower[i]%2==0){
+              count+=lower[i];
+            }
+            else{
+                count+=lower[i]-1;
+                odd=1;
+            }
+            if(upper[i]%2==0){
+                count+=upper[i];
+
+            }
+            else{
+              count+=upper[i]-1;
+              odd=1;
+            }
         }
-    }
-    int count=0;
-    int odd=0;
-    for(int i=0;i<26;i++){
-        //for lower
-        if(lower[i]%2==0){
-            count+=lower[i];               //even
-        }
-        else{
-            count+=lower[i]-1;
-            odd=1;
-        }
-         if(upper[i]%2==0){
-            count+=upper[i];
-        }
-        else{
-            count+=upper[i]-1;
-            odd=1;
-        }
+return count+odd;
         
-    }
-    return count+odd;
     }
 };
