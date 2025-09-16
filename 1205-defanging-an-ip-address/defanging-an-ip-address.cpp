3623780629ecc58b08e58c3a5c93db027f69414a
abhideveloper007 <148200371+abhideveloper007@users.noreply.github.com>
2025-09-17 03:54:47 +0530

    Time: 2 ms (52.61%) | Memory: 7.6 MB (76.17%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         string temp;
-        vector<string>ans;
         int i=0;
         while(i<address.size()){
             if(address[i]=='.'){
-                
-                
-                temp+='[';
-                temp+='.';
-                temp+=']';
-                ans.push_back(temp);
-                temp.clear();
+              temp+="[.]";
 
             }
             else{
@@ -20,15 +13,7 @@ public:
             }
             i++;
         }
-        if(!temp.empty()){
-
-            ans.push_back(temp);
-            temp.clear();
-        }
-  
-    for(int i=0;i<ans.size();i++){
-     temp+=ans[i];
-    }
-    return temp;
+        return temp;
+        
     }
 };
